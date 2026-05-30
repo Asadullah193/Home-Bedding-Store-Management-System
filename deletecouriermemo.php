602a57c975d87ce27378d10dd7d3ccf09d83e932
Asadullah Shakil <104208181+Asadullah193@users.noreply.github.com>
2026-05-30 17:16:19 +0600

    Delete deletecouriermemo.php

--- deletecouriermemo.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("data_class.php");
-
-$deletecourier_memoid=$_GET['deletecourier_memoid'];
-
-
-$obj=new data();
-$obj->setconnection();
-$obj->deletecouriermemo($deletecourier_memoid);
